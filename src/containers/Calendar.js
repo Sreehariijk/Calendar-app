cf1442f0b41cdc1aad7e180558dc1390891973dd
P <679806@cognizant.com>
2018-07-09 12:12:34 +0530

    latest changes

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -4,12 +4,18 @@ import Logo from '../components/Logo';
 
 class Calendar extends Component {
   render() {
+    console.log("test");
     return (
       <div className="calendar">
           <August title="AUGUST 2018"/>
           <Logo />
+      
+      <div className="calendar">
+          <August title="AUGUST 2018"/>
+          <Logo />
       </div>
-     
+</div>
+
     );
   }
 }
